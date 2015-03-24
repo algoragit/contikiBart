ba1767acb8050b649300928dcb94301140871aab
Bart Lemmens <mailbartl@gmail.com>
2015-03-24 23:19:48 +0100

    basic rdc functionality implementation

--- core/net/mac/muchmac/muchmac.c
+++ core/net/mac/muchmac/muchmac.c
@@ -5,7 +5,30 @@
 static void
 send(mac_callback_t sent_callback, void *ptr)
 {
+  int ret;
 
+  packetbuf_set_addr(PACKETBUF_ADDR_SENDER, &linkaddr_node_addr);
+
+  if (NETSTACK_FRAMER.create_and_secure() < 0) {
+    ret = MAC_TX_ERR_FATAL;
+  } else {
+    switch (NETSTACK_RADIO.send(packetbuf_hdrptr(), packetbuf_totlen())) {
+    case RADIO_TX_OK:
+      ret = MAC_TX_OK;
+      break;
+    case RADIO_TX_COLLISION:
+      ret = MAC_TX_COLLISION;
+      break;
+    case RADIO_TX_NOACK:
+      ret = MAC_TX_NOACK;
+      break;
+    default:
+      ret = MAC_TX_ERR;
+      break;
+    }
+  }
+
+  mac_call_sent_callback(sent_callback, ptr, ret, 1);
 }
 
 static void
@@ -17,7 +40,12 @@ send_list(mac_callback_t sent_callback, void *ptr, struct rdc_buf_list *list)
 static void
 input(void)
 {
-
+  if (NETSTACK_FRAMER.parse() >= 0) {
+    if (linkaddr_cmp(packetbuf_addr(PACKETBUF_ADDR_RECEIVER), &linkaddr_node_addr) ||
+        packetbuf_holds_broadcast()) {
+      NETSTACK_MAC.input();
+    }
+  }
 }
 
 static int
@@ -45,7 +73,7 @@ channel_check_interval(void)
 static void
 init(void)
 {
-
+  on();
 }
 
 const struct rdc_driver muchmac_driver = {
