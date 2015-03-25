66cfb67e095c6cbf243c2aa4d7200ccba3f478aa
Bart Lemmens <mailbartl@gmail.com>
2015-03-25 11:39:37 +0100

    postpone packet transmission

--- core/net/mac/muchmac/muchmac.c
+++ core/net/mac/muchmac/muchmac.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include "net/rime/timesynch.h"
 #include "dev/leds.h"
+#include "lib/memb.h"
+#include "lib/list.h"
 
 #define ON_PERIOD	RTIMER_SECOND/2
 
@@ -12,10 +14,27 @@ static struct ctimer ct;
 static struct pt pt;
 static struct rtimer rt;
 
+// queue
+struct muchmac_queue_item {
+  struct muchmac_queue_item *next;
+  struct queuebuf *buf;
+  mac_callback_t sent_callback;
+  void *sent_ptr;
+};
+
+#define MAX_QUEUE_SIZE	4
+MEMB(muchmac_queue_memb, struct muchmac_queue_item, MAX_QUEUE_SIZE);
+LIST(muchmac_queue);
+
+PROCESS(send_packet, "muchmac send process");
+
+static volatile unsigned char radio_is_on = 0;
+
 static int
 turn_on()
 {
   leds_on(LEDS_BLUE);
+  radio_is_on = 1;
   return NETSTACK_RADIO.on();
 }
 
@@ -23,6 +42,7 @@ static int
 turn_off()
 {
   leds_off(LEDS_BLUE);
+  radio_is_on = 0;
   return NETSTACK_RADIO.off();
 }
 
@@ -34,6 +54,9 @@ powercycle(struct rtimer *t, void *ptr)
   while (1) {
     // slot start
     turn_on();
+    if (list_tail(muchmac_queue) != NULL) {
+      process_poll(&send_packet);
+    }
     rtimer_set(t, RTIMER_NOW() + ON_PERIOD, 0, (rtimer_callback_t)powercycle, NULL);
     PT_YIELD(&pt);
 
@@ -54,6 +77,58 @@ powercycle(struct rtimer *t, void *ptr)
   }
 
   PT_END(&pt);
+}
+
+
+#define WAIT_FOR_SEND	RTIMER_SECOND/10
+
+PROCESS_THREAD(send_packet, ev, data)
+{
+  PROCESS_BEGIN();
+
+  while (1) {
+    PROCESS_YIELD_UNTIL(ev == PROCESS_EVENT_POLL);
+
+    if (list_tail(muchmac_queue) != NULL) {
+      // busy wait to account for clock drift
+      rtimer_clock_t t0 = RTIMER_NOW();
+      while (RTIMER_CLOCK_LT(RTIMER_NOW(), t0 + WAIT_FOR_SEND)) ;
+
+      struct muchmac_queue_item *item = list_tail(muchmac_queue);
+      queuebuf_to_packetbuf(item->buf);
+
+      printf("muchmac: send packet %p from queue\n", item->buf);
+
+      int ret;
+      packetbuf_set_addr(PACKETBUF_ADDR_SENDER, &linkaddr_node_addr);
+
+      if (NETSTACK_FRAMER.create_and_secure() < 0) {
+        ret = MAC_TX_ERR_FATAL;
+      } else {
+        switch (NETSTACK_RADIO.send(packetbuf_hdrptr(), packetbuf_totlen())) {
+        case RADIO_TX_OK:
+          ret = MAC_TX_OK;
+          break;
+        case RADIO_TX_COLLISION:
+          ret = MAC_TX_COLLISION;
+          break;
+        case RADIO_TX_NOACK:
+          ret = MAC_TX_NOACK;
+          break;
+        default:
+          ret = MAC_TX_ERR;
+          break;
+        }
+      }
+
+      mac_call_sent_callback(item->sent_callback, item->sent_ptr, ret, 1);
+
+      list_remove(muchmac_queue, item);
+      memb_free(&muchmac_queue_memb, item);
+    }
+  }
+
+  PROCESS_END();
 }
 
 static void
@@ -89,8 +164,22 @@ static void
 send_list(mac_callback_t sent_callback, void *ptr, struct rdc_buf_list *list)
 {
   if (list != NULL) {
-    queuebuf_to_packetbuf(list->buf);
-    send(sent_callback, ptr);
+    if (radio_is_on) {
+      queuebuf_to_packetbuf(list->buf);
+      send(sent_callback, ptr);
+    } else {
+      struct muchmac_queue_item* item;
+      item = memb_alloc(&muchmac_queue_memb);
+      if (item == NULL) {
+        mac_call_sent_callback(sent_callback, ptr, MAC_TX_ERR_FATAL, 1);
+      } else {
+        item->buf = list->buf;
+        item->sent_callback = sent_callback;
+        item->sent_ptr = ptr;
+        list_push(muchmac_queue, item);
+        printf("packet %p queued\n", item->buf);
+      }
+    }
   }
 }
 
@@ -140,6 +229,11 @@ init(void)
   on();
 
   PT_INIT(&pt);
+
+  memb_init(&muchmac_queue_memb);
+  list_init(muchmac_queue);
+
+  process_start(&send_packet, NULL);
 
   // schedule start of TDMA mode
   ctimer_set(&ct, 120*CLOCK_SECOND, start_tdma, NULL);
